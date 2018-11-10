e4515ab5640e822d472548dd2b6f53447914cd14
AlbanCrepel <alban.crepel@aboutgoods.net>
2018-11-11 00:25:30 +0100

    :wrench: setting up the webpack config and the plugin file

---
+++ src/plugin.js
@@ -0,0 +1,7 @@
+import VueDisplacementSlideshow from './VueDisplacementSlideshow.vue';
+
+export default {
+    install: function (Vue, options) {
+        Vue.component('vue-displacement-slideshow', VueDisplacementSlideshow);
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+
+var config = {
+    output: {
+        path: path.resolve(__dirname + '/dist/'),
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: __dirname,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!less!css'
+            }
+        ]
+    },
+    externals: {
+        vue: 'vue'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin( {
+            minimize : true,
+            sourceMap : false,
+            mangle: true,
+            compress: {
+                warnings: false
+            }
+        } )
+    ]
+};
+
+
+module.exports = [
+    merge(config, {
+        entry: path.resolve(__dirname + './src/plugin.js'),
+        output: {
+            filename: 'vue-displacement-slideshow.min.js',
+            libraryTarget: 'window',
+            library: 'VueDisplacementSlideshow',
+        }
+    }),
+    merge(config, {
+        entry: path.resolve(__dirname + './src/VueDisplacementSlideshow.vue'),
+        output: {
+            filename: 'vue-displacement-slideshow.js',
+            libraryTarget: 'umd',
+            library: 'vue-displacement-slideshow',
+            umdNamedDefine: true
+        }
+    })
+];
